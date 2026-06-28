7a6260f659dacd780ac7f8650268ce566ce06830
thakurarvindra675-jpg <thakurarvindra675@gmail.com>
2026-06-28 16:18:05 +0530

    Render compatiblity 2

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/backup', require('./routes/backupRoutes'));
 // Serve frontend in production
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
 });
 
